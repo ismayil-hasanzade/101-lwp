5b4419dee7fa4d8a26a0fa9961d055e8391343c1
Ismayil Hasanzade <126759096+ismayil-hasanzade@users.noreply.github.com>
2023-04-13 05:12:30 +0400

    13 Complated

---
+++ 13-ArrayObject/script.js
@@ -0,0 +1,26 @@
+const arr = [23, 1, 5, 3, 12, 4, 9, 81, 7, 11, 22, 31, 6];
+console.log(arr);
+let max = -Infinity;
+let min = Infinity;
+let max_index;
+let min_index;
+let memo;
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] % 2 == 0) {
+    if (arr[i] > max) {
+      max = arr[i];
+      max_index = i;
+    }
+  } else {
+    if (arr[i] < min) {
+      min = arr[i];
+      min_index = i;
+    }
+  }
+}
+
+memo = arr[max_index];
+arr[max_index] = arr[min_index];
+arr[min_index] = memo;
+
+console.log(arr);
