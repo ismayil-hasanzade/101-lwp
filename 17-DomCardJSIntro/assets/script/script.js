9b56b2c0dd1270ee5c85438cdb453cf241b66112
Ismayil Hasanzade <126759096+ismayil-hasanzade@users.noreply.github.com>
2023-04-23 23:48:56 +0400

    17 complated

--- 17-DomCardJSIntro/assets/script/script.js
+++ 17-DomCardJSIntro/assets/script/script.js
@@ -3,26 +3,28 @@ const color_1 = document.querySelector(".color-1");
 const color_2 = document.querySelector(".color-2");
 const stars = document.querySelector(".stars");
 const sizes = document.querySelector(".sizes");
+const many = document.querySelector(".many");
 color_2.addEventListener("click", () => {
   container.classList.add("change");
+  many.innerHTML = "$6300";
 });
 color_1.addEventListener("click", () => {
   container.classList.remove("change");
+  many.innerHTML = "$1800";
 });
 
 for (let i = 0; i < sizes.children.length; i++) {
+  sizes.children[0].style.cssText = `
+   background-color:#25d393;
+   color:#fff`;
+
   sizes.children[i].addEventListener("click", () => {
     for (let j = 0; j < sizes.children.length; j++) {
       sizes.children[j].style.cssText = "";
     }
     sizes.children[i].style.cssText = `
-        width: 2rem;
-        height: 2rem;
         background-color: #25d393;
         color: #fff;
-        border-radius: 50%;
-        display: grid;
-        place-items: center;
       `;
   });
 }
